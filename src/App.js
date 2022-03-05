e71a617fe0f63c30b18a5ef9d7ddd9fc1c467b36
Deep Mandlik <mandlikdeep20@gmail.com>
2022-03-06 00:24:46 +0530

    feat: made seperate component for webcam

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./App.css";
 
-import Webcam from "react-webcam";
+import WebCam from './components/webcam/webcam';
 
 function App() {
-  const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
 
   return (
     <div className="App">
       <header className="App-header">
-        <Webcam
-          ref={webcamRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
-
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
+        <WebCam />
       </header>
     </div>
   );
